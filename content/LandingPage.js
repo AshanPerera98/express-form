350dc6908a30fbfa29c34d59a9f55c0116c5c1d3
Ashan Perera <ashantiwankaperera@gmail.com>
2025-02-04 21:33:30 +0530

    image optimization for landing page

--- content/LandingPage.js
+++ content/LandingPage.js
@@ -20,9 +20,9 @@ const categoryCards = [
 
 // --------------------------------------------------------
 
-import slide1 from "@/assets/LandingPage/Carousel/slide1.jpg";
-import slide2 from "@/assets/LandingPage/Carousel/slide2.jpg";
-import slide3 from "@/assets/LandingPage/Carousel/slide3.jpg";
+import slide1 from "@/assets/LandingPage/Carousel/slide1.webp";
+import slide2 from "@/assets/LandingPage/Carousel/slide2.webp";
+import slide3 from "@/assets/LandingPage/Carousel/slide3.webp";
 
 const carouselSlides = [
   {
