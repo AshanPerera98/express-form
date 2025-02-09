2092a4355159d6834668f0254e548ac9b99fba9e
Ashan Perera <ashantiwankaperera@gmail.com>
2025-02-09 18:18:04 +0530

    minor content update

--- content/LandingPage.js
+++ content/LandingPage.js
@@ -26,19 +26,19 @@ import slide3 from "@/assets/LandingPage/Carousel/slide3.webp";
 
 const carouselSlides = [
   {
-    title: "First Slide",
+    title: "Kitchen Countertop",
     description:
       "Our countertops are crafted with durability in mind. Designed to withstand the rigors of daily life, they’re heat, scratch, and stain-resistant—perfect for busy family kitchens or the adventurous lifestyle of caravan living. With our benchtops, you can enjoy long-lasting beauty and functionality for years to come.",
     image: slide1.src,
   },
   {
-    title: "Second Slide",
+    title: "Cafe Tabletop",
     description:
       "Our wide selection of materials, colors, and finishes allows you to customize your countertops to match your unique style. Whether you prefer a sleek, modern look or a more rustic charm, we have the perfect option to complement your space. Enjoy the perfect balance of aesthetics and performance with countertops designed to elevate any room.",
     image: slide2.src,
   },
   {
-    title: "Third Slide",
+    title: "Stove Lidtop",
     description:
       "We make the process easy and stress-free with professional installation services. Our experienced team ensures your countertops are installed quickly, safely, and to the highest standards. Whether you're upgrading your home or renovating your caravan, you can relax knowing the job will be done right, every time.",
     image: slide3.src,
